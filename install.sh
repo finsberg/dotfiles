71dee258be28696974a1e7ca42f6054d7221c2dd
Henrik Finsberg <henriknf@simula.no>
2019-04-28 18:48:19 +0200

    Create separate script for creating symlinks

--- install.sh
+++ install.sh
@@ -4,31 +4,13 @@
 # And also installs Homebrew Packages and Anaconda
 ############################
 
-# dotfiles directory
-dotfiledir=${HOME}/local/src/dotfiles
-
-# list of files/folders to symlink in ${homedir}
-files="bash_profile bashrc bash_prompt aliases paths"
-
-# change to the dotfiles directory
-echo "Changing to the ${dotfiledir} directory"
-cd ${dotfiledir}
-echo "...done"
-
-# create symlinks (will overwrite old dotfiles)
-for file in ${files}; do
-    echo "Creating symlink to $file in home directory."
-    ln -sf ${dotfiledir}/.${file} ${HOME}/.${file}
-done
+./symlinks.sh
 
 # Download Git Auto-Completion
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${HOME}/.git-completion.bash
 
 # Run the Homebrew Script
 ./brew.sh
-
-# Now create symlink to emacs file
-ln -sf ${dotfiledir}/.emacs ${HOME}/.emacs.d/init.el
 
 # Install miniconda
 curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh | bash
---
+++ symlinks.sh
@@ -0,0 +1,21 @@
+# dotfiles directory
+dotfiledir=${HOME}/local/src/dotfiles
+
+# list of files/folders to symlink in ${homedir}
+files="bash_profile bashrc bash_prompt aliases paths"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${HOME}/.${file}
+done
+
+# Now create symlink to emacs file
+mkdir -p ${HOME}/.emacs.d/
+echo "Creating symlink to emacs file"
+ln -sf ${dotfiledir}/.emacs ${HOME}/.emacs.d/init.el
